41e4bb529d40ff37059e4e5436cef431bef53fdd
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-06 19:07:58 -0700

    Declared a variable for buttons to access node list of buttons. Use forEach method on buttons node list to house event listener for individual buttons. Console.log string of text to text. Test works on click.

--- script.js
+++ script.js
@@ -33,27 +33,10 @@ function operate(operator, a, b) {
     };
 };
 
+let buttons = document.querySelectorAll('button');
 
-    // declare variable for row containers
-    const rows = document.querySelectorAll('.row');
-
-    // iterate through node list of rows
-    rows.forEach(row => {
-        // add click listener for the row 
-        // declare variable for buttons
-        const buttons = document.querySelectorAll('button')
-        buttons.forEach(button => {
-            button.addEventListener('click', () => {
-                // // select button text content
-                // let content = button.textContent();
-                // // assign display text content variable
-                // let displayCont = '';
-                // // assign button text content to display text content
-                // displayCont.textContent((`${content}`));
-                console.log('hello');
-            });
-        });
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log('kopi');
     });
-
-    // store button in variable
-    // show number on display
+});
