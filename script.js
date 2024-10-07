c2bdba815b96dcf3b91bb7d05860e28c3599bd9b
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-06 18:45:47 -0700

    comment out logic for button event listener. console.log('hello') on button click to test if button listener is working.

--- script.js
+++ script.js
@@ -33,18 +33,27 @@ function operate(operator, a, b) {
     };
 };
 
-function btnClick(button) {
-    // click button event listener
-    let button = document.querySelectorAll('button');
-    let btnContent = btn.textContent;
-    button.addEventListener('click', (e) => {
-        console.log(hey);
-        // store display in variable
-        let disp = document.querySelector('.display');
-        // change display text content to button text content
-        // store display value in a variable
+
+    // declare variable for row containers
+    const rows = document.querySelectorAll('.row');
+
+    // iterate through node list of rows
+    rows.forEach(row => {
+        // add click listener for the row 
+        // declare variable for buttons
+        const buttons = document.querySelectorAll('button')
+        buttons.forEach(button => {
+            button.addEventListener('click', () => {
+                // // select button text content
+                // let content = button.textContent();
+                // // assign display text content variable
+                // let displayCont = '';
+                // // assign button text content to display text content
+                // displayCont.textContent((`${content}`));
+                console.log('hello');
+            });
+        });
     });
+
     // store button in variable
     // show number on display
-};
-
