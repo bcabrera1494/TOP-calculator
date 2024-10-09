b3bc33b4a9aed799ac90484042cd9786f046c629
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-09 11:11:44 -0700

    Add if statement under mouseup event listener for if btnText === +/- to break.

--- script.js
+++ script.js
@@ -82,24 +82,6 @@ buttons.forEach(button => {
                     break;
                 } else {display.textContent = display.textContent.concat(button.textContent) };
                 break;
-                // // if (target.textContent === '+/-') {
-                // //     if (display.textContent.includes('-' === true)) {
-                // //         input.num1 = display.textContent.slice(-1);
-                // //         input.operator = target.textContent
-                // //         let num1 = input.num1;
-                // //         let op = input.operator;
-                // //         let answer = operate(op, num1);
-                // //         display.textContent = answer;
-                // //     } else {
-                // //         input.num1 = display.textContent;
-                // //         input.operator = target.textContent;
-                // //         let num1 = Number(input.num1);
-                // //         let op = input.operator;
-                // //         let answer = operate(op, num1);
-                // //         display.textContent = answer;
-                // //     };
-                //     break;
-                // } 
             case 'clear':
                 if (target.id === 'backspace' && display.textContent.length > 1) {
                     display.textContent = display.textContent.slice(0, -1);
@@ -184,6 +166,9 @@ buttons.forEach(button => {
                 ){  let displayText = display.textContent;
                     input.num1 = displayText.slice(0, opsIndex);
                     input.num2 = displayText.slice(opsIndex);
+                };
+                if (btnText === '+/-'){
+                    break;
                 };
                 // ) {
                 //     if (display.textContent[0] === '-' && target.textContent == '+/-'){
